b74d9990a19a6112c71b675f51da0709aa3e2a82
Artur Trzop <arturtrzop@gmail.com>
2024-01-04 15:47:01 +0100

    Update 1_spec.rb

--- spec/something/1_spec.rb
+++ spec/something/1_spec.rb
@@ -10,4 +10,14 @@ describe 'something 1' do
   it do
     expect(true).to be true
   end
+
+  context 'awesome context' do
+    it do
+      expect(true).to be true
+    end
+
+    it do
+      expect(true).to be true
+    end
+  end
 end
